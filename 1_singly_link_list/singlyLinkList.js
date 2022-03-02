465d66b5b3e3358e08230c3edd12e11944321a73
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2022-03-02 16:34:00 +0100

    Singly Link List: Pop

--- 1_singly_link_list/singlyLinkList.js
+++ 1_singly_link_list/singlyLinkList.js
@@ -26,12 +26,37 @@ class SinglyLinkList {
 
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
 }
 
 const list = new SinglyLinkList();
 list.push('hey');
 list.push('how');
 list.push('are');
-list.push('you');
+list.pop();
+list.pop();
+list.pop();
 
-console.log(list.head.next.next.next);
+console.log(list);
