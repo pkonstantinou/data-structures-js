0e5074eda59c1ac1a735a75bfa0f38709d2038c1
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2022-03-03 14:14:32 +0100

    Stack: Push

---
+++ 3_stacks_and_queues/stack.js
@@ -0,0 +1,29 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+
+    return ++this.size;
+  }
+}
