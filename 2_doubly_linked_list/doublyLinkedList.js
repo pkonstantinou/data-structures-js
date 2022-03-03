adcb037f0b4fa780252305096673bc3b46a7c8ae
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2022-03-03 10:37:31 +0100

    Doubly Linked List: Push

--- 2_doubly_linked_list/doublyLinkedList.js
+++ 2_doubly_linked_list/doublyLinkedList.js
@@ -12,4 +12,26 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
 }
+
+const list = new DoublyLinkedList();
+list.push('here');
+list.push('we');
+list.push('are');
+console.log(list);
