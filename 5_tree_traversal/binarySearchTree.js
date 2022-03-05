2e1cec1a6495449c338cd4bf88af47a1492c473a
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2022-03-05 17:46:03 +0100

    Tree Traversal: Breadth First Search

---
+++ 5_tree_traversal/binarySearchTree.js
@@ -0,0 +1,86 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+
+    while (true) {
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else if (value > current.value) {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      } else {
+        return undefined;
+      }
+    }
+  }
+
+  contains(value) {
+    if (this.root === null) return false;
+
+    let current = this.root;
+
+    while (true) {
+      if (value < current.value) {
+        if (current.left === null) return false;
+        current = current.left;
+      } else if (value > current.value) {
+        if (current.right === null) return false;
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+  }
+
+  BFS() {
+    const data = [];
+    const queue = [];
+    let node = this.root;
+
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    return data;
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(3);
+tree.insert(8);
+tree.insert(15);
+tree.insert(20);
+console.log(tree.BFS());
