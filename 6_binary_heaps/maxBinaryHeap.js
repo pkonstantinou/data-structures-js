f665044aa05a153128cdb636226a81e582e27c12
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2022-03-06 16:23:49 +0100

    Binary Heaps: ExtractMax

--- 6_binary_heaps/maxBinaryHeap.js
+++ 6_binary_heaps/maxBinaryHeap.js
@@ -1,6 +1,6 @@
 class MaxBinaryTree {
   constructor() {
-    this.values = [41, 39, 33, 18, 27, 12];
+    this.values = [];
   }
 
   insert(element) {
@@ -23,9 +23,76 @@ class MaxBinaryTree {
       idx = parentIdx;
     }
   }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+
+    if (this.values.lenght > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
 }
 
 let heap = new MaxBinaryTree();
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
 heap.insert(55);
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
+console.log(heap.extractMax());
+console.log(heap.values);
 // [41, 39, 33, 18, 27, 12]
 //  0   1   2   3   4   5
